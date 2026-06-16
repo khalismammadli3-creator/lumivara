43ec41f93f9edd6f3ed2fa5b3e4a2d3c3f1902fe
Khalis <khalismammadli3@gmail.com>
2026-06-16 14:16:04 +0400

    added workflow scripts

---
+++ done.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host "Type a short description of what changed, then press Enter:"
+$msg = Read-Host
+git add .
+git commit -m $msg
+git push
+Write-Host ""
+Write-Host "Done! Your changes are saved to GitHub."
---
+++ sync.ps1
@@ -0,0 +1,4 @@
+﻿Write-Host "Pulling latest changes from GitHub..."
+git pull
+Write-Host ""
+Write-Host "Done! Your files are up to date. Safe to use Claude Code now."
